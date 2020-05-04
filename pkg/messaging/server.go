ad5484ffd47f72cd2c958de9dbc02ac615d57df5
Michal Fronczyk <fronczyk@gmail.com>
2020-05-04 09:03:09 +0200

    Take room data from json request body.

--- pkg/messaging/server.go
+++ pkg/messaging/server.go
@@ -1,6 +1,7 @@
 package messaging
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -20,11 +21,26 @@ func NewRoomServer(store RoomStore) *RoomServer {
 
 func (s *RoomServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	router := mux.NewRouter()
-	router.HandleFunc("/rooms/{uid:.{1,40}}", s.CreateRoom)
+	router.HandleFunc("/rooms", s.CreateRoom).Methods("POST")
 	router.ServeHTTP(w, r)
 }
 
+type CreateRoomReq struct {
+	UID string `json:"uid"`
+}
+
 func (s *RoomServer) CreateRoom(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	s.store.CreateRoom(vars["uid"])
+	var req CreateRoomReq
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "decoding json failed", http.StatusBadRequest)
+		return
+	}
+
+	if len(req.UID) < 1 || len(req.UID) > 40 {
+		http.Error(w, "uid: must be between 1 and 40 characters", http.StatusBadRequest)
+		return
+	}
+
+	s.store.CreateRoom(req.UID)
 }
--- pkg/messaging/server_test.go
+++ pkg/messaging/server_test.go
@@ -1,6 +1,8 @@
 package messaging_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -19,18 +21,45 @@ func (s *SpyRoomStore) CreateRoom(uid string) {
 
 func TestCreateRoom(t *testing.T) {
 	t.Run("creates empty room with given id", func(t *testing.T) {
-		room := "abc-123"
+		req := messaging.CreateRoomReq{UID: "123-abc"}
+		body, _ := json.Marshal(req)
 		store := &SpyRoomStore{}
 		server := messaging.NewRoomServer(store)
-		request, _ := http.NewRequest(http.MethodPost, "/rooms/"+room, nil)
+		request, _ := http.NewRequest(http.MethodPost, "/rooms", bytes.NewBuffer(body))
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
-		assert.Equal(t, http.StatusOK, response.Result().StatusCode, "should return status success")
+		assert.Equal(t, http.StatusOK, response.Result().StatusCode, "should return success")
 		if assert.Equal(t, 1, len(store.rooms), "should create single room") {
-			assert.Equal(t, room, store.rooms[0], "room id should match")
+			assert.Equal(t, req.UID, store.rooms[0], "room id should match")
 		}
+	})
+
+	t.Run("returns error when UID too long", func(t *testing.T) {
+		req := messaging.CreateRoomReq{UID: "0123456789-0123456789-0123456789-0123456789"}
+		body, _ := json.Marshal(req)
+		store := &SpyRoomStore{}
+		server := messaging.NewRoomServer(store)
+		request, _ := http.NewRequest(http.MethodPost, "/rooms", bytes.NewBuffer(body))
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Result().StatusCode, "should return bad request")
+		assert.Equal(t, 0, len(store.rooms), "should not create any rooms")
+	})
+
+	t.Run("returns error when invalid request", func(t *testing.T) {
+		store := &SpyRoomStore{}
+		server := messaging.NewRoomServer(store)
+		request, _ := http.NewRequest(http.MethodPost, "/rooms", bytes.NewBuffer([]byte{0}))
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Result().StatusCode, "should return bad request")
+		assert.Equal(t, 0, len(store.rooms), "should not create any rooms")
 	})
 
 }
