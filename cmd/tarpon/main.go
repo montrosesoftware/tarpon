a5ab70feb2dc2c191f2f963d7645f511caaa40ef
Michal Fronczyk <fronczyk@gmail.com>
2020-05-14 23:02:43 +0200

    Use memory store in tarpon cmd.

--- cmd/tarpon/main.go
+++ cmd/tarpon/main.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/montrosesoftware/tarpon/pkg/messaging"
 	"github.com/montrosesoftware/tarpon/pkg/server"
 )
 
 func main() {
-	server := server.RoomServer{}
-	if err := http.ListenAndServe(":5000", &server); err != nil {
+	store := messaging.NewRoomStore()
+	server := server.NewRoomServer(store)
+	log.Printf("Server working...")
+	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("can't listen on port 5000 %v", err)
 	}
 }
